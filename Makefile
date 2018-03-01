88d89ec9ada94f5311ed85fe39de46873e0d0059
shawn <qxoo0604@gmail.com>
2018-03-02 00:01:36 +0800

    update

---
+++ Makefile
@@ -0,0 +1,3 @@
+stub:
+	python -m grpc_tools.protoc -I ./ --python_out=./ \
+	--grpc_python_out=./ ./grpc_plugin/proto/test.proto
--- grpc_plugin/autodiscover.py
+++ grpc_plugin/autodiscover.py
@@ -1,5 +1,8 @@
 import inspect
 from .decorators import BaseGrpc
+import os
+import grpc_plugin.proto
+import importlib
 
 
 def autodiscover_grpc(installed_apps):
@@ -8,8 +11,18 @@ def autodiscover_grpc(installed_apps):
     """
     grpc_cls_list = []
     for app in installed_apps:
-        for name, obj in inspect.getmembers('.'.join([app, 'grpc_view'])):
+        module = importlib.import_module('.'.join([app, 'grpc_view']))
+        for name, obj in inspect.getmembers(module):
             if inspect.isclass(obj):
                 if issubclass(obj, BaseGrpc):
                     grpc_cls_list.append(obj)
     return grpc_cls_list
+
+
+def autodiscover_grpc_service():
+    """
+    """
+    file_list = os.listdir(grpc_plugin.proto.__path__[0])
+    grpc_list = [fl[:-3] for fl in file_list if fl.endswith('grpc.py')]
+    for grpc_f in grpc_list:
+        inspect.getmembers('grpc_plugin.proto.%s' % grpc_f)
