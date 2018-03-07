c55f9e3b48cb1fc9936371a4883697b210a1fc33
zhangyu <yu.zhang@jian24.com>
2018-03-07 09:55:48 +0800

    update

--- grpc_plugin/client.py
+++ grpc_plugin/client.py
@@ -2,8 +2,11 @@ from .key import get_key
 import grpc
 
 
-def client_channel(addrport):
-    trusted_certs = get_key.server_crt()
-    credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
-    channel = grpc.secure_channel(addrport, credentials)
+def client_channel(addrport, ssl=False):
+    if ssl:
+        trusted_certs = get_key.server_crt()
+        credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+        channel = grpc.secure_channel(addrport, credentials)
+    else:
+        channel = grpc.insecure_channel(addrport)
     return channel
